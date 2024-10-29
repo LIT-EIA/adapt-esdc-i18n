8237d36d23a57c7f809815831adfe059d825e828
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2024-10-29 16:40:20 -0400

    reintegrated value interpolation

--- assets/i18n.js
+++ assets/i18n.js
@@ -1,13 +1,14 @@
-$.i18n.translate = function (key, mode) {
+$.i18n.translate = function (key, options) {
+  var mode = options && options.mode;
   var messages = this().messageStore.messages;
   var override = messages.override && messages.override[key];
   var lang = (mode === 'aat') ? localStorage.getItem("lang") || 'en' : document.documentElement.lang;
   this().locale = lang;
-  return (mode !== 'aat') && override || this(key);
+  return (mode !== 'aat') && override || this(key, options.$1);
 };
 
-Handlebars.registerHelper('t', function (key) {
-  return $.i18n.translate(key);
+Handlebars.registerHelper('t', function (key, $1) {
+  return $.i18n.translate(key, {$1: $1});
 });
 
 Handlebars.registerHelper('tp', function (string, key) {
@@ -15,9 +16,9 @@ Handlebars.registerHelper('tp', function (string, key) {
 });
 
 Handlebars.registerHelper('ta', function (key, $1) {
-  return $.i18n.translate(key, 'aat');
+  return $.i18n.translate(key, {$1: $1, mode: 'aat'});
 });
 
 Handlebars.registerHelper('tpa', function (string, key) {
-  return $.i18n.translate(string + key, 'aat');
+  return $.i18n.translate(string + key, {mode: 'aat'});
 });
