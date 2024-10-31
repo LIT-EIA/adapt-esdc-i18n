15830f78db155a09189ff11e31b825e3e5540f18
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2024-10-31 16:29:09 -0400

    added support for handlebars strings

--- assets/i18n.js
+++ assets/i18n.js
@@ -1,23 +1,26 @@
 $.i18n.translate = function (key, options) {
   var mode = options && options.mode;
-  var $1 = options && options.$1;
+  var data = options && options.data;
   var messages = this().messageStore.messages;
   var override = messages.override && messages.override[key];
   var lang = (mode === 'aat') ? localStorage.getItem("lang") || 'en' : document.documentElement.lang;
   this().locale = lang;
-  return (mode !== 'aat') && override || this(key, $1);
+  var string = (mode !== 'aat') && override || this(key, data);
+  var template = Handlebars.compile(string);
+  var translation = template(data);
+  return translation;
 };
 
-Handlebars.registerHelper('t', function (key, $1) {
-  return $.i18n.translate(key, {$1: $1});
+Handlebars.registerHelper('t', function (key, data) {
+  return $.i18n.translate(key, {data: data});
 });
 
 Handlebars.registerHelper('tp', function (string, key) {
   return $.i18n.translate(string + key);
 });
 
-Handlebars.registerHelper('ta', function (key, $1) {
-  return $.i18n.translate(key, {$1: $1, mode: 'aat'});
+Handlebars.registerHelper('ta', function (key, data) {
+  return $.i18n.translate(key, {data: data, mode: 'aat'});
 });
 
 Handlebars.registerHelper('tpa', function (string, key) {
